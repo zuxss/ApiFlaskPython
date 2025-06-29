9e22c9abf23cbac4a99a45bf50ac1eb0489092c3
Franco Pacheco <pachecozux@gmail.com>
2025-06-29 17:43:43 -0300

    update cors error

--- servidor.py
+++ servidor.py
@@ -1,8 +1,13 @@
+from flask_cors import CORS
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from utils import hash_password, verify_password, validar_datos
 
 app = Flask(__name__)
+CORS(app) 
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
